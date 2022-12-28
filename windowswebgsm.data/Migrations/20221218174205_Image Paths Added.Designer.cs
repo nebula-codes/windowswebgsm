448860f2be85d7ce5c232fcdd2002fa3bc25e206
Nebula-Codes <drosd02@tutanota.com>
2022-12-27 21:45:59 -0500

    no message

--- windowswebgsm.data/Migrations/20221218174205_Image Paths Added.Designer.cs
+++ windowswebgsm.data/Migrations/20221218174205_Image Paths Added.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using windowswebgsm.data;
 
@@ -10,9 +11,11 @@ using windowswebgsm.data;
 namespace windowswebgsm.data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221218174205_Image Paths Added")]
+    partial class ImagePathsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
